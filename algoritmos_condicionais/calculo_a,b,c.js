509b0cd5de704b722d0bec4d8aed9a78e405a1fe
LetíciaSouza <119368474+leticiasouza-dev@users.noreply.github.com>
2023-06-13 15:22:01 -0300

    adicionando e resolvendo mais dois exercicios de estruturas condicionais em javascript

---
+++ algoritmos_condicionais/calculo_a,b,c.js
@@ -0,0 +1,12 @@
+// 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
+// que C
+
+let a = 5;
+let b = 9;
+let c = 10;
+
+if (a + b < c) {
+    console.log('A soma de A + B é menor que C');
+} else {
+    console.log('A soma de A + B é maior que C');
+}
---
+++ algoritmos_condicionais/identificação_casamento.js
@@ -0,0 +1,17 @@
+// 2) Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. Caso sexo seja “F” e
+// estado civil seja “CASADA”, solicitar o tempo de casada (anos). 
+
+let nome = "maria";
+let genero = "feminino";
+let estadoCivil = "casada";
+
+if(genero === "feminino" && estadoCivil === "casada"){
+    console.log("informe quanto anos está casada");
+} else{
+    console.log("ok");
+}
+
+
+
+
+
